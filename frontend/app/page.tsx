71e570783856a39b96f7d576d6d9df5cbb8b07a3
Resham <reshamupadhyay48@gmail.com>
2025-12-10 23:23:59 +0530

    fe init

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+export default function Home() {
+  return (
+    <div className="text-white">
+    <WalletMultiButton/></div>
+  );
+}
