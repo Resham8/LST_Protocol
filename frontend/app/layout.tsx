f7926af96d0580e13e39c6e13504240331f1b6b5
Resham <reshamupadhyay48@gmail.com>
2025-12-14 21:51:44 +0530

    wallet

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "@solana/wallet-adapter-react-ui/styles.css";
-import { SolanaProvider } from "./providers/SolanaProvider";
+import { SolanaProvider } from "../providers/SolanaProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,10 @@
-"use client"
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+"use client";
+import StakingCard from "@/components/staking-card";
 
 export default function Home() {
   return (
-    <div className="text-white">
-    <WalletMultiButton/></div>
+    <div className="min-h-screen bg-linear-to-br from-[oklch(0.16_0.06_180)] via-[oklch(0.18_0.05_170)] to-[oklch(0.15_0.07_160)] flex items-center justify-center p-4">
+      <StakingCard/>
+    </div>
   );
 }
